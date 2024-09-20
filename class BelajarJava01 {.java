3daa530f9c9aa2a41255957fab113be28b8bfc6d
444arrival <rheivaladhitia@gmail.com>
2024-09-20 14:19:05 +0700

    Tugas bu Rizza jurnal-02

---
+++ class BelajarJava01 {.java
@@ -0,0 +1,7 @@
+class BelajarJava01 {
+    public static void main(String []args){
+        System.out.println("Nama Saya Rheival");
+        System.out.println("NIM : 6070624300086");
+        System.out.println("Kelas D3IF - 48 - 01");
+    }
+}
